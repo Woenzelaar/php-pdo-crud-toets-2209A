9acc5f0a39e7bb9922b6708ca747da8b221bb0fb
WESSEL SMIT <WESSEL@SMIT-ONLINE.NL>
2023-01-10 11:22:04 +0100

    !

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./img/form.ico" type="image/x-icon">
+    <title>PDO CRUD TOETS</title>
+</head>
+<body>
 
-echo "Hallo wereld";
+    <h1>PDO CRUD TOETS</h1>
 
-?>
+    
+</body>
+</html>
