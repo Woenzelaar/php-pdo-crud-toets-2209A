bd2a2e0d03b8fabab87896b009673a747a3002ae
WESSEL SMIT <WESSEL@SMIT-ONLINE.NL>
2023-01-10 11:29:58 +0100

    !

--- read.php
+++ read.php
@@ -1,5 +1,21 @@
 <?php
 
+include('config.php');
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        echo "Connected successfully";
+    }
+    else {
+        echo "Connection failed";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
 
 
 ?>
